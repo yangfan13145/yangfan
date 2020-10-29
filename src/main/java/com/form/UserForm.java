7d89cc5cc41d08944f78d66f4a1da12abf612aa6
Admin <835757321@qq.com>
2020-10-29 16:43:18 +0800

    update UserForm

--- src/main/java/com/form/UserForm.java
+++ src/main/java/com/form/UserForm.java
@@ -4,4 +4,12 @@ public class UserForm {
     private int id;
     private String name;
     private String sex;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
